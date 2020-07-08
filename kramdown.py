b49c9eb79d575ba44241bb44a57281480095e638
Andrzej Drabarek <andrzej.drabarek@protonmail.com>
2020-07-08 10:38:26 +0200

    Cleaned up the code, handling of temporary files through tempfile module.

--- kramdown.py
+++ kramdown.py
@@ -1,9 +1,8 @@
 import sublime_plugin
+import tempfile
 import os
 
-TMP_DIR = '/tmp'
-
-HEADER = """<!DOCTYPE html>
+HEADER = b"""<!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
@@ -16,46 +15,22 @@ HEADER = """<!DOCTYPE html>
 <body>
 """
 
-FOOTER = """</body>
+FOOTER = b"""</body>
 </html>
 """
 
-class cd:
-		# A convenience class for changing working directory for a single
-		# scope.
-
-    def __init__(self, newPath):
-        self.newPath = os.path.expanduser(newPath)
-
-    def __enter__(self):
-        self.savedPath = os.getcwd()
-        os.chdir(self.newPath)
-
-    def __exit__(self, etype, value, traceback):
-        os.chdir(self.savedPath)
-
-class HtmlShow(sublime_plugin.TextCommand):
+class RenderKramdownInBrowser(sublime_plugin.TextCommand):
 	def run(self, edit):
-		if not os.path.exists(TMP_DIR):
-			os.makedirs(TMP_DIR)
-			
-		if self.view.file_name() != None:
-			print(os.getcwd())
-								
-			with cd(TMP_DIR):
-				fn = self.view.file_name()
-				
-				command = '/usr/local/bin/kramdown ' + os.path.abspath(fn)
-				print(command)
-				stream = os.popen(command)
-				output = stream.read()
-				print(output)
+		fn = self.view.file_name()
 
-				html_path = TMP_DIR + "/" + os.path.basename(fn) + ".html"
-				with open(html_path, 'w') as f:
-					f.write(HEADER)
-					f.write(output)
-					f.write(FOOTER)
+		command = 'kramdown ' + os.path.abspath(fn)
+		stream = os.popen(command)
+		output = stream.read()
 
-				command = 'sensible-browser ' + html_path + '&'
-				stream = os.popen(command)
+		with tempfile.NamedTemporaryFile(delete=False, suffix='.html') as f:
+			f.write(HEADER)
+			f.write(bytes(output, 'utf-8'))
+			f.write(FOOTER)
+			command = 'sensible-browser ' + f.name + '&'
+
+		stream = os.popen(command)
